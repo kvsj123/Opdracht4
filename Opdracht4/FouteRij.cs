10460a7d656a7b9e09ff9cbcf2bc98f8c1266aec
kvsj123 <kristian.vasiaj@student.ehb.be>
2022-10-23 22:44:31 +0200

    Add project files.

---
+++ Opdracht4/FouteRij.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht4
+{
+    internal class FouteRij<T>
+    {
+        Queue<T> rij_original = new Queue<T>();
+        Queue<T> rij_copy = new Queue<T>();
+
+        private T Huidig;
+
+        public T Toevoegen(T iets)
+        {
+            if (rij_original.Count == 0)
+            {
+                rij_original.Enqueue(iets);
+                Huidig = iets;
+            }
+            else
+            {
+                rij_original.Enqueue(iets);
+            }
+            return Huidig;
+        }
+
+        public T Verwijderen()
+        {
+            rij_original.Dequeue();
+            Huidig = rij_original.Peek();
+            return Huidig;
+        }
+
+        public T Volgende()
+        {
+            Huidig = rij_original.Peek();
+
+            return Huidig;
+        }    
+        
+        public void Leegmaken()
+        {
+            rij_original.Clear();
+            Huidig = default(T);
+        }
+
+        public T ZetAchteraan()
+        {
+            T el = rij_original.Peek();
+            rij_original.Dequeue();
+            Huidig = rij_original.Peek();
+            rij_original.Enqueue(el);
+
+            return Huidig;
+        }
+
+        public override string ToString()
+        {
+            string showRij = "";
+
+            foreach(T iets in rij_original)
+            {
+                showRij += iets.ToString() + "\n";
+            }
+
+            return showRij;
+        }
+
+        public Queue<T> Copy()
+        {
+            rij_copy = rij_original;
+
+            return rij_copy;
+        }
+
+    }
+
+
+}
---
+++ Opdracht4/TeDoen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht4
+{
+    internal class TeDoen
+    {
+        private static int Id;
+        private DateTime? Tijdstip;
+        private string Titel;
+        private string[] Informatie;
+        
+
+        public TeDoen(string titel, string[] Informatie, DateTime tijdStip)
+        {
+            Id++;
+
+            if(Tijdstip >= DateTime.Now)
+            {
+                Tijdstip = tijdStip;
+            }
+            else
+            {
+                Tijdstip = null;
+            }
+
+            this.Titel = titel;
+            this.Informatie = Informatie;
+
+
+        }
+
+        public override string ToString()
+        {
+            string ToonAlles = "";
+
+            foreach (string el in Informatie)
+            {
+                ToonAlles += "- " +  el.ToString() + "\n";
+
+            }
+
+            if (Tijdstip == null)
+            {
+                return "Id :" + Id + "    Titel : " + Titel + "\n -----------------------" + "\n Informatie : " + ToonAlles;
+            }
+            else
+            {
+                return "Id :" + Id + "    Titel : " + Titel + " \n Tijdstip: " + Tijdstip + "\n -----------------------" +"\n Informatie : " + ToonAlles;
+            }
+        }
+    }
+}
